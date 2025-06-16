5fae8f8992468a27157284ba147be533ba32f215
aditya24-TI <cahyo24ti@mahasiswa.pcr.ac.idcom>
2025-06-16 15:00:41 +0700

    nambah file pertama kali

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP dan MYSQL - Menampilkan data dari DATABASE MYSQL dengan PHP </title>
+</head>
+<body>
+    <h1>Data kafe </h1>
+    <table class="table table-bordere">
+        <thead>
+            <th>id meja</th>
+            <th>nomor meja</th>
+            <th>kapasitas</th>
+        </thead>
+        <?php 
+        $data = mysqli_query($conn,"SELECT * FROM meja");
+        while ($row = mysqli_fetch_array($data)){
+        ?>
+        <tr>
+            <td><?php echo $row['id_meja'];?></td>
+            
+        <td>
+            <?php echo $row['nomor_meja'];?>
+        </td>
+        
+        
+        <td>
+            <?php echo $row['kapasitas'];?>
+        </td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+</body>
+</html>
